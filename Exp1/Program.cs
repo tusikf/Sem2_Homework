c7ec2ae1942667ffce76a1f887a814b18a07b3d1
Nataly <tusikf@yandex.ru>
2023-06-15 22:00:01 +0300

    done 1  exesize

--- Exp1/Program.cs
+++ Exp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. (Проверка на трехзначность при вводе пользователем)
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = 1000;
+
+Console.Write("Введите трехзначное число1: ");
+number = int.Parse(Console.ReadLine()!);
+int kk = number / 100;
+
+while ( kk < 1 || kk > 10)
+{
+    Console.Write("Введите трехзначное число: ");
+    number = int.Parse(Console.ReadLine()!);
+    kk = number / 100;
+}
+
+int num1 = number % 100;
+int num2 = num1 / 10;
+
+Console.WriteLine($"Вторая цифра введенного числа это : { num2 }");
